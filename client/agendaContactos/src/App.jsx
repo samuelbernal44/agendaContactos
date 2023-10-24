2063040819b467019664f0e41768d2639929d31b
samuelbernal44 <samuelbernal44@gmail.com>
2023-10-23 22:55:28 -0500

    Petición de datos al servidor

--- client/agendaContactos/src/App.jsx
+++ client/agendaContactos/src/App.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { Landing, Home, Detail, Form } from './views';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
+  const [data, setData] = useState([]);
   const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/contact');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
   return (
     <>
       {pathname !== '/' && <NavBar />}
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home data={data} />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/create" element={<Form />} />
       </Routes>
