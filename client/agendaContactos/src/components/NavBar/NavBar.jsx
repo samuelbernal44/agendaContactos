bb5ad7f87e906652a2a726e48391236bf81663c3
samuelbernal44 <samuelbernal44@gmail.com>
2023-10-23 22:56:54 -0500

    estilos básicos

--- client/agendaContactos/src/components/NavBar/NavBar.jsx
+++ client/agendaContactos/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import Container from '../Style/StyledNavBar';
 
 const NavBar = () => {
   return (
-    <div>
+    <Container>
       <Link to="/home">Home</Link>
       <Link to="/create">Form</Link>
-    </div>
+    </Container>
   );
 };
 
---
+++ client/agendaContactos/src/components/Style/StyledNavBar.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  border-radius: 15px;
+  border: 1px solid black;
+`;
+
+export default Container;
